27e6077de36754b5c43746c1bac933136876ee96
Arinjay Dhar <87296817+Arinji2@users.noreply.github.com>
2024-03-31 18:19:30 +0530

    Fix SRC

--- src/utils/cookieHelpers.ts
+++ src/utils/cookieHelpers.ts
@@ -1,7 +1,12 @@
 "use server";
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { cookies } from "next/headers";
-export async function AppendCookie(key: string, value: string) {
-  cookies().set(key, value);
+export async function AppendCookie(
+  key: string,
+  value: string,
+  options?: Partial<ResponseCookie>
+) {
+  cookies().set(key, value, options);
 }
 export async function HasCookie(key: string) {
   return cookies().has(key);
