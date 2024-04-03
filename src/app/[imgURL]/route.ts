47d68d3658ae53881f39c6d1daee5ab14a15379b
Arinjay Dhar <87296817+Arinji2@users.noreply.github.com>
2024-04-03 11:03:12 +0530

    Finish Img Redirects

---
+++ src/app/[imgURL]/route.ts
@@ -0,0 +1,27 @@
+import initPocketbase from "@/lib/initPocketbase";
+import { notFound } from "next/navigation";
+import sharp from "sharp";
+
+export async function GET({ params }: { params: { imgURL: string } }) {
+  const pb = await initPocketbase();
+  try {
+    const image = await pb
+      .collection("emojis")
+      .getFirstListItem(`url="${params.imgURL}" && prefixed=false`);
+
+    const url = `https://db-imagee.arinji.com/api/files/emojis/${image.id}/${image.emoji}?`;
+
+    const fetchedImage = await fetch(url, { cache: "force-cache" });
+    const buffer = await fetchedImage.arrayBuffer();
+    const resizedImage = await sharp(buffer)
+      .resize(48, 48, { fit: "contain" })
+      .toBuffer();
+    return new Response(resizedImage, {
+      headers: {
+        "Content-Type": "image/png",
+      },
+    });
+  } catch (error) {
+    notFound();
+  }
+}
--- src/app/img/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  const imageUrl =
-    "https://db-imagee.arinji.com/api/files/wj5zb3eupeq5t2l/5fqxcd2p9a8cxbd/wat_G6Rz9CscFj.png";
-  const data = await fetch(imageUrl, { cache: "no-cache" });
-  return new Response(data.body);
-}
